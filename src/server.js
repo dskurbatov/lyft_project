80ac7531dba2f9e91f11077d99c88046cb246651
Dima Kurbatov <dskurbatov13@gmail.com>
2019-04-25 16:27:33 -0700

    removed if statement from the handler, which handles post requests to /test endpoint

--- src/server.js
+++ src/server.js
@@ -7,12 +7,8 @@ const app = new express()
 app.use(bodyParser.json())
 
 app.post('/test', (req, res) => {
-  if(!req.body.string_to_cut || typeof req.body.string_to_cut !== 'string'){
-    res.json({"error": 'No "string_to_cut" has been provided or provided not a string'})
-  } else {
-    const str = transfrom(req.body.string_to_cut)
-    res.json({"return_string": str})
-  }
+  const str = transfrom(req.body.string_to_cut)
+  res.json({"return_string": str})
 })
 
 module.exports = app
