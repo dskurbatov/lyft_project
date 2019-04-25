e0e59cd67aff89ba68045fa431d120026dc593f6
Dima Kurbatov <dskurbatov13@gmail.com>
2019-04-25 16:27:47 -0700

    added some comments

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
+//external
 const express = require('express')
 const bodyParser = require('body-parser')
+
+//internal
 const transfrom = require('./transform')
 
 const app = new express()
 
+//middleware
 app.use(bodyParser.json())
 
+//routes
 app.post('/test', (req, res) => {
   const str = transfrom(req.body.string_to_cut)
   res.json({"return_string": str})
